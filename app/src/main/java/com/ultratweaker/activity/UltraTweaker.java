c251679b16acc9251c88b44bb61d34dd2603510d
Ahmed Hady <ahmedhady6@gmail.com>
2017-04-13 20:10:32 +0200

    Implement Selinux Switcher

--- app/src/main/java/com/ultratweaker/activity/UltraTweaker.java
+++ app/src/main/java/com/ultratweaker/activity/UltraTweaker.java
@@ -3,21 +3,31 @@ package com.ultratweaker.activity;
 import android.annotation.SuppressLint;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceFragment;
 import android.preference.PreferenceManager;
+import android.preference.SwitchPreference;
 
 import com.ultratweaker.R;
+import com.ultratweaker.utils.du.CMDProcessor;
 
-public class UltraTweaker extends PreferenceActivity {
+public class UltraTweaker extends PreferenceActivity implements Preference.OnPreferenceChangeListener {
+    private SwitchPreference mSelinux;
+    private static final String SELINUX = "selinux";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getFragmentManager().beginTransaction().replace(android.R.id.content, new MyPreferenceFragment()).commit();
     }
 
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        return false;
+    }
+
     @SuppressLint("ValidFragment")
-    public class MyPreferenceFragment extends PreferenceFragment {
+    public class MyPreferenceFragment extends PreferenceFragment implements Preference.OnPreferenceChangeListener {
         SharedPreferences mPrefs;
 
         @Override
@@ -31,7 +41,32 @@ public class UltraTweaker extends PreferenceActivity {
             super.onCreate(savedInstanceState);
             addPreferencesFromResource(R.xml.main_page);
 
+            //SELinux
+            mSelinux = (SwitchPreference) findPreference(SELINUX);
+            mSelinux.setOnPreferenceChangeListener(this);
+
+            if (CMDProcessor.runShellCommand("getenforce").getStdout().contains("Enforcing")) {
+                mSelinux.setChecked(true);
+                mSelinux.setSummary(R.string.selinux_enforcing_summary);
+            } else {
+                mSelinux.setChecked(false);
+                mSelinux.setSummary(R.string.selinux_permissive_summary);
+            }
         }
 
+        @Override
+        public boolean onPreferenceChange(Preference preference, Object newValue) {
+            if (preference == mSelinux) {
+                if (newValue.toString().equals("true")) {
+                    CMDProcessor.runSuCommand("setenforce 1");
+                    mSelinux.setSummary(R.string.selinux_enforcing_summary);
+                } else if (newValue.toString().equals("false")) {
+                    CMDProcessor.runSuCommand("setenforce 0");
+                    mSelinux.setSummary(R.string.selinux_permissive_summary);
+                }
+                return true;
+            }
+            return false;
+        }
     }
 }
